e56ffbf65f976824fc6a09e80465001929f1af37
Thaynara Lobo Amaral <thaynara.loboo@hotmail.com>
2024-03-13 18:42:44 -0300

    feat: Connecting to Database

--- RestApi/RestApi/Program.cs
+++ RestApi/RestApi/Program.cs
@@ -1,8 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using RestApi.Model.Context;
+using RestApi.Services.Implementations;
+using RestApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
+    connection,
+    new MySqlServerVersion(new Version(8, 0, 29)))
+);
+
+//Dependency Injection
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
 
 var app = builder.Build();
 
---
+++ RestApi/RestApi/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using RestApi.Model;
+
+namespace RestApi.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
